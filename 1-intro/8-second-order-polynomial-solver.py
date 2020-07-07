b6a3c9f202d115a04c87a3bca1b82007cb012f19
MarcoSelvatici <marcoselvatici98@gmail.com>
2020-07-07 11:07:56 +0200

    Add another exercise to lecture 1.

---
+++ 1-intro/8-second-order-polynomial-solver.py
@@ -0,0 +1,7 @@
+# A second order polynomial is a polynomial like:
+# x^2 + 3x + 7
+# In this exercise, you have to write a program that asks the user the coefficients
+# for the polynomial and then shows its solutions.
+# Note you can use the square root by doing: sqrt(...)
+
+from math import sqrt # Needed to use the sqrt function.
